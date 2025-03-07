3a5bb7a0e991363f0f94a94985d18c6a2d4aa38c
장원정 <jang_expedition@gmail.com>
2025-03-07 17:43:10 +0900

    feat: api 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.enableCors({
     origin: ['http://localhost:5173'],
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
